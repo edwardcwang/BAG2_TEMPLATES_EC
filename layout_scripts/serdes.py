9b0ac968f306d18a1bdb2c996958debbb0ca5881
Eric Chang <pkerichang@berkeley.edu>
2017-03-28 18:13:10 -0700

    SerdesRXBase: 1. fix bug that if diffamp sign is negative, offset cancellation PMOS gates are flipped. RXCore: 1. change tap1 bias pin name to match schematic. layout_scripts: updating rxcore layout generation script.

--- layout_scripts/serdes.py
+++ layout_scripts/serdes.py
@@ -14,30 +14,14 @@ def rxcore(prj, temp_db):
 
     cell_name = 'rxcore_ffe1_dfe4'
 
-    layout_params = dict(
+    params = dict(
         lch=16e-9,
-        ptap_w=6,
-        ntap_w=6,
-        hm_width=1,
-        hm_cur_width=2,
-        diff_space=1,
-        sig_widths=[1, 2],
-        sig_spaces=[2, 2],
-        clk_widths=[2, 3, 4],
-        clk_spaces=[2, 3, 6],
-        sig_clk_spaces=[2, 3],
-        min_fg_sep=4,
         w_dict={'load': 6, 'casc': 6, 'in': 4, 'sw': 4, 'tail': 4},
         th_dict={'load': 'ulvt', 'casc': 'ulvt', 'in': 'ulvt', 'sw': 'ulvt', 'tail': 'svt'},
         integ_params={'load': 4, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
         alat_params_list=[
             {'load': 2, 'casc': 8, 'in': 6, 'sw': 6, 'tail': 4},
             {'load': 2, 'casc': 8, 'in': 6, 'sw': 6, 'tail': 4},
-        ],
-        dlat_params_list=[
-            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
-            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
-            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
         ],
         intsum_params=dict(
             fg_load=12,
@@ -59,55 +43,43 @@ def rxcore(prj, temp_db):
             ],
             sgn_list=[1, -1],
         ),
+        dlat_params_list=[
+            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
+            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
+            {'load': 6, 'casc': 4, 'in': 4, 'sw': 4, 'tail': 2},
+        ],
     )
+
+    layout_params = dict(
+        ptap_w=6,
+        ntap_w=6,
+        hm_width=1,
+        hm_cur_width=2,
+        diff_space=1,
+        sig_widths=[1, 2],
+        sig_spaces=[2, 2],
+        clk_widths=[2, 3, 4],
+        clk_spaces=[2, 3, 6],
+        sig_clk_spaces=[2, 3],
+        min_fg_sep=4,
+    )
+
+    layout_params.update(params)
 
     pprint.pprint(layout_params)
     template = temp_db.new_template(params=layout_params, temp_cls=RXCore, debug=False)
     print('total number of fingers: %d' % template.num_fingers)
     temp_db.instantiate_layout(prj, template, cell_name, debug=True)
+    return params, template.num_fingers
 
 
-def rxcore_sch(prj):
+def rxcore_sch(prj, sch_params, tot_fg):
     lib_name = 'serdes_bm_templates'
     cell_name = 'rxcore_ffe1_dfe4'
 
-    params = dict(
-        lch=16e-9,
-        w_dict={'load': 6, 'casc': 6, 'in': 4, 'sw': 4, 'tail': 6},
-        th_dict={'load': 'ulvt', 'casc': 'ulvt', 'in': 'lvt', 'sw': 'ulvt', 'tail': 'ulvt'},
-        integ_params={'load': 8, 'casc': 8, 'in': 4, 'sw': 4, 'tail': 8},
-        alat_params_list=[
-            {'load': 12, 'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-            {'load': 10, 'casc': 10, 'in': 6, 'sw': 6, 'tail': 10},
-        ],
-        dlat_params_list=[
-            {'load': 12, 'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-            {'load': 12, 'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-            {'load': 12, 'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-        ],
-        intsum_params=dict(
-            fg_load=36,
-            gm_fg_list=[
-                {'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-                {'casc': 8, 'in': 4, 'sw': 4, 'tail': 8},
-                {'casc': 4, 'in': 4, 'sw': 4, 'tail': 4},
-                {'casc': 4, 'in': 4, 'sw': 4, 'tail': 4},
-                {'casc': 8, 'in': 4, 'sw': 4, 'tail': 8},
-            ],
-            sgn_list=[1, -1, -1, -1, -1],
-        ),
-        summer_params=dict(
-            fg_load=16,
-            gm_fg_list=[
-                {'casc': 12, 'in': 8, 'sw': 8, 'tail': 12},
-                {'casc': 10, 'in': 6, 'sw': 6, 'tail': 10},
-            ],
-            sgn_list=[1, -1],
-        ),
-    )
-    pprint.pprint(params)
+    pprint.pprint(core_params)
     dsn = prj.create_design_module(lib_name, cell_name)
-    dsn.design_specs(**params)
+    dsn.design_specs(fg_tot=2 * tot_fg, **sch_params)
     dsn.implement_design(impl_lib, top_cell_name=cell_name, erase=True)
 
 
@@ -127,7 +99,7 @@ if __name__ == '__main__':
 
         tdb = TemplateDB('template_libs.def', routing_grid, impl_lib, use_cybagoa=True)
 
-        rxcore(bprj, tdb)
-        # rxcore_sch(bprj)
+        core_params, fg_tot = rxcore(bprj, tdb)
+        rxcore_sch(bprj, core_params, fg_tot)
     else:
         print('loading BAG project')
