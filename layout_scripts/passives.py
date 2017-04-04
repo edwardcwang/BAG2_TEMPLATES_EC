bb0fd726ddede36d5b10339358257085a2517c1e
Eric Chang <pkerichang@berkeley.edu>
2017-04-03 23:08:23 -0700

    SubstrateContact: 1. added default_top_layer() class method to query lowest valid top layer.
    cap module:
    1. added MOMCap template class that can be used to characterize MOM cap in this technology.

--- layout_scripts/passives.py
+++ layout_scripts/passives.py
@@ -4,9 +4,10 @@ import pprint
 
 from bag import BagProject
 from abs_templates_ec.passives.hp_filter import HighPassFilterDiff
+from abs_templates_ec.passives.cap import MOMCap
 from bag.layout import RoutingGrid, TemplateDB
 
-impl_lib = 'AAAFOO_hpf'
+impl_lib = 'AAAFOO_cap'
 
 
 def hpf(prj, temp_db):
@@ -33,6 +34,27 @@ def hpf(prj, temp_db):
     template = temp_db.new_template(params=layout_params, temp_cls=HighPassFilterDiff, debug=False)
     temp_db.instantiate_layout(prj, template, 'hpfilter', debug=True)
 
+
+def mom(prj, temp_db):
+    # type: (BagProject, TemplateDB) -> None
+
+    layout_params = dict(
+        cap_bot_layer=4,
+        cap_top_layer=7,
+        cap_width=3.0,
+        cap_height=6.0,
+        sub_lch=16e-9,
+        sub_w=6,
+        sub_type='ptap',
+        threshold='ulvt',
+        show_pins=True,
+    )
+
+    pprint.pprint(layout_params)
+    template = temp_db.new_template(params=layout_params, temp_cls=MOMCap, debug=False)
+    temp_db.instantiate_layout(prj, template, 'momcap', debug=True)
+
+
 if __name__ == '__main__':
 
     local_dict = locals()
@@ -49,6 +71,7 @@ if __name__ == '__main__':
 
         tdb = TemplateDB('template_libs.def', routing_grid, impl_lib, use_cybagoa=True)
 
-        hpf(bprj, tdb)
+        # hpf(bprj, tdb)
+        mom(bprj, tdb)
     else:
         print('loading BAG project')
