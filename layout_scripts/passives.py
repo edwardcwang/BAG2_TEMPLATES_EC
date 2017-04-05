c16bb2a8109fbc0365a9706eecb9f0241cdc96a8
Eric Chang <pkerichang@berkeley.edu>
2017-04-05 10:01:27 -0700

    AnalogBase: make internal routing grid override parent layers.
    ResArrayBase:
    1. make internal routing grid override parent layers.
    2. allow optional internal routing layers.
    
    HighPassFilter:
    done first pass modification.

--- layout_scripts/passives.py
+++ layout_scripts/passives.py
@@ -3,11 +3,11 @@
 import pprint
 
 from bag import BagProject
-from abs_templates_ec.passives.hp_filter import HighPassFilterDiff
+from abs_templates_ec.passives.hp_filter import HighPassFilter
 from abs_templates_ec.passives.cap import MOMCap
 from bag.layout import RoutingGrid, TemplateDB
 
-impl_lib = 'AAAFOO_cap'
+impl_lib = 'AAAFOO_hpf'
 
 
 def hpf(prj, temp_db):
@@ -16,12 +16,9 @@ def hpf(prj, temp_db):
     layout_params = dict(
         l=10e-6,
         w=0.36e-6,
-        cap_port_width=2,
-        cap_edge_margin=0.5e-6,
-        cap_diff_margin=0.5e-6,
+        cap_edge_margin=0.5,
         num_seg=2,
-        num_cap_layer=4,
-        tr_idx_list=[6.5, 3.5],
+        num_cap_layer=3,
         io_width=2,
         sub_lch=16e-9,
         sub_w=6,
@@ -31,7 +28,7 @@ def hpf(prj, temp_db):
     )
 
     pprint.pprint(layout_params)
-    template = temp_db.new_template(params=layout_params, temp_cls=HighPassFilterDiff, debug=False)
+    template = temp_db.new_template(params=layout_params, temp_cls=HighPassFilter, debug=False)
     temp_db.instantiate_layout(prj, template, 'hpfilter', debug=True)
 
 
@@ -62,16 +59,16 @@ if __name__ == '__main__':
         print('creating BAG project')
         bprj = BagProject()
         temp = 70.0
-        layers = [4, 5, 6, 7]
-        spaces = [0.084, 0.080, 0.084, 0.080]
-        widths = [0.060, 0.100, 0.060, 0.100]
-        bot_dir = 'x'
+        layers = [3, 4, 5, 6, 7]
+        spaces = [0.04, 0.084, 0.080, 0.084, 0.080]
+        widths = [0.05, 0.060, 0.100, 0.060, 0.100]
+        bot_dir = 'y'
 
         routing_grid = RoutingGrid(bprj.tech_info, layers, spaces, widths, bot_dir)
 
         tdb = TemplateDB('template_libs.def', routing_grid, impl_lib, use_cybagoa=True)
 
-        # hpf(bprj, tdb)
-        mom(bprj, tdb)
+        hpf(bprj, tdb)
+        # mom(bprj, tdb)
     else:
         print('loading BAG project')
