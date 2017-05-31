e22d07c79e84ff6acd2676d2bbfa39646a6e7268
pkerichang <pkerichang@gmail.com>
2017-05-31 08:04:18 -0700

    misc: 1. update LaygoBase test script.
    MOSTech:
    1. added is_laygo parameter to draw_substrate_connection().  This way
       we can reuse templates.
    
    AnalogSubstrateConn:
    1. added is_laygo parameter.
    
    AnalogEdge:
    1. added is_laygo parameter.
    
    AnalogBase:
    1. update AnalogSubstrateConn parameter.
    
    LaygoPrimitive/LaygoSubstrate
    1. added get_end_flags() method so we know how to properly draw edge
       blocks.
    
    LaygoBase:
    1. added end flags tracking data structure, and extension boundary
       edge drawing.

--- test_scripts/laygobase.py
+++ test_scripts/laygobase.py
@@ -79,6 +79,9 @@ class Test(LaygoBase):
         """
         self.set_row_types(['nch'], ['R0'], ['lvt'], True, 15)
         self.add_laygo_primitive('fg2d', w=4, loc=(0, 0), nx=4, spx=1)
+        self.set_laygo_size()
+        self.fill_space()
+        self.draw_boundary_cells()
 
 
 def make_tdb(prj, target_lib, specs):
