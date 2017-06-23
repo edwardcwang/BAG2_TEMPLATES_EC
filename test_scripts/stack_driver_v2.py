305ad7b7b49a97c28914f1fdc990d3a245bd9cad
pkerichang <pkerichang@gmail.com>
2017-06-23 15:11:44 -0700

    DigitalBase: 1. added implementation for add_digital_block().
    LaygoBase:
    1. added config parameter to digital row information dictionary.
    2. change get_end_info() return type.
    
    added StackDriverArray layout test script.

--- test_scripts/stack_driver_v2.py
+++ test_scripts/stack_driver_v2.py
@@ -34,6 +34,7 @@ from bag.layout.routing import RoutingGrid, TrackID
 from bag.layout.template import TemplateDB
 
 from abs_templates_ec.laygo.core import LaygoBase
+from abs_templates_ec.digital.core import DigitalBase
 
 
 class StackDriver(LaygoBase):
@@ -196,6 +197,71 @@ class StackDriver(LaygoBase):
         return core_warrs, sup_warrs
 
 
+class StackDriverArray(DigitalBase):
+    """A single diff amp.
+
+    Parameters
+    ----------
+    temp_db : TemplateDB
+            the template database.
+    lib_name : str
+        the layout library name.
+    params : Dict[str, Any]
+        the parameter values.
+    used_names : Set[str]
+        a set of already used cell names.
+    **kwargs
+        dictionary of optional parameters.  See documentation of
+        :class:`bag.layout.template.TemplateBase` for details.
+    """
+
+    def __init__(self, temp_db, lib_name, params, used_names, **kwargs):
+        # type: (TemplateDB, str, Dict[str, Any], Set[str], **Any) -> None
+        super(StackDriverArray, self).__init__(temp_db, lib_name, params, used_names, **kwargs)
+
+    @classmethod
+    def get_params_info(cls):
+        """Returns a dictionary containing parameter descriptions.
+
+        Override this method to return a dictionary from parameter names to descriptions.
+
+        Returns
+        -------
+        param_info : dict[str, str]
+            dictionary from parameter name to description.
+        """
+        return dict(
+            driver_params='stack driver parameters.',
+            nx='number of columns.',
+            ny='number of rows.',
+            show_pins='True to draw pin geometries.',
+        )
+
+    def draw_layout(self):
+        """Draw the layout of a dynamic latch chain.
+        """
+        driver_params = self.params['driver_params']
+        nx = self.params['nx']
+        ny = self.params['ny']
+        show_pins = self.params['show_pins']
+
+        draw_boundaries = False
+        end_mode = 15
+
+        drv_master = self.new_template(params=driver_params, temp_cls=StackDriver)
+        row_info = drv_master.get_digital_row_info()
+
+        self.initialize(row_info, ny, draw_boundaries, end_mode)
+
+        spx = drv_master.laygo_size[0]
+        for row_idx in range(ny):
+            self.add_digital_block(drv_master, loc=(0, row_idx), nx=nx, spx=spx)
+
+        num_col = nx * spx
+        self.set_digital_size(num_col)
+        self.draw_boundary_cells()
+
+
 def make_tdb(prj, target_lib, specs):
     grid_specs = specs['routing_grid']
     layers = grid_specs['layers']
@@ -208,8 +274,11 @@ def make_tdb(prj, target_lib, specs):
     return tdb
 
 
-def generate(prj, specs):
+def generate_unit(prj):
     lib_name = 'AAAFOO'
+
+    with open('test_specs/stack_driver_v2.yaml', 'r') as f:
+        specs = yaml.load(f)
 
     params = specs['params']
 
@@ -222,16 +291,32 @@ def generate(prj, specs):
     print('done')
 
 
-if __name__ == '__main__':
+def generate_array(prj):
+    lib_name = 'AAAFOO'
 
-    with open('test_specs/stack_driver_v2.yaml', 'r') as f:
-        block_specs = yaml.load(f)
+    with open('test_specs/stack_driver_array.yaml', 'r') as f:
+        specs = yaml.load(f)
+
+    params = specs['params']
+
+    temp_db = make_tdb(prj, lib_name, specs)
+
+    template = temp_db.new_template(params=params, temp_cls=StackDriverArray, debug=False)
+    name = 'STACK_DRIVER_ARR'
+    print('create layout')
+    temp_db.batch_layout(prj, [template], [name])
+    print('done')
+
+
+if __name__ == '__main__':
 
     local_dict = locals()
     if 'bprj' not in local_dict:
         print('creating BAG project')
         bprj = BagProject()
-
-        generate(bprj, block_specs)
     else:
         print('loading BAG project')
+        bprj = local_dict['bprj']
+
+    # generate_unit(bprj)
+    generate_array(bprj)
